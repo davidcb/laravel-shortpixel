39b4e40b882072346d5a6cfd50d2de968ecf0c44
David Couto <davidcouto@gmail.com>
2018-10-09 09:55:58 +0100

    Added ServiceProvider

---
+++ src/Davidcb/LaravelShortPixel/LaravelShortPixelServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Davidcb\LaravelShortPixel;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelShortPixelServiceProvider extends ServiceProvider
+{
+    protected $defer = true;
+
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__.'/../../config/shortpixel.php' => config_path('shortpixel.php'),
+        ], 'config');
+    }
+
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/../../config/shortpixel.php', 'shortpixel');
+
+        $this->app->singleton('laravel-shortpixel', function ($app) {
+            return new LaravelShortPixel($app);
+        });
+    }
+
+    public function provides()
+    {
+        return ['laravel-shortpixel'];
+    }
+}
